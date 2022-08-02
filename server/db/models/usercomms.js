c6ad20458f1ad6bc2b814a33a43d3829939a38fc
Denis Gutsulak <d.99113@gmail.com>
2022-08-02 17:20:59 +0300

    UserComms us->com -> us

--- server/db/models/usercomms.js
+++ server/db/models/usercomms.js
@@ -1,14 +1,9 @@
-'use strict';
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class UserComms extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ Users, Notifications }) {
       this.belongsTo(Users, { foreignKey: 'user_id' });
       this.belongsTo(Users, { foreignKey: 'couch_id' });
